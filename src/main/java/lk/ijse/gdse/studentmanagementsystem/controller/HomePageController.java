a5222743c4ce33825417b8c68cc331ec7aac9835
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-06-09 22:45:11 +0530

    create home page

--- src/main/java/lk/ijse/gdse/studentmanagementsystem/controller/HomePageController.java
+++ src/main/java/lk/ijse/gdse/studentmanagementsystem/controller/HomePageController.java
@@ -1,4 +1,81 @@
 package lk.ijse.gdse.studentmanagementsystem.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class HomePageController {
+
+    @FXML
+    private Button btnAttendance;
+
+    @FXML
+    private Button btnCOURSE;
+
+    @FXML
+    private Button btnDepartment;
+
+    @FXML
+    private Button btnLec;
+
+    @FXML
+    private Button btnLogOut;
+
+    @FXML
+    private Button btnPayment;
+
+    @FXML
+    private Button btnSalary;
+
+    @FXML
+    private Button btnStudent;
+
+    @FXML
+    private AnchorPane homeAnchorpane;
+
+
+    @FXML
+    private AnchorPane loadingAnchorpane;
+
+    @FXML
+    void attendanceOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnActoin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void courseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void departmentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lectuersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void paymentOnActition(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salaryOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void studentOnAction(ActionEvent event) {
+
+    }
+
 }
