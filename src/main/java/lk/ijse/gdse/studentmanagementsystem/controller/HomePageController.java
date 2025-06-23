af661b0c342b1512b6a7616579c0d90f1ada5024
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-06-23 12:53:53 +0530

    update home page controller

--- src/main/java/lk/ijse/gdse/studentmanagementsystem/controller/HomePageController.java
+++ src/main/java/lk/ijse/gdse/studentmanagementsystem/controller/HomePageController.java
@@ -2,8 +2,12 @@ package lk.ijse.gdse.studentmanagementsystem.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+
+import javax.swing.*;
+import java.io.IOException;
 
 public class HomePageController {
 
@@ -75,7 +79,22 @@ public class HomePageController {
 
     @FXML
     void studentOnAction(ActionEvent event) {
+        navigateTo("/view/studentView.fxml");
 
+    }
+
+
+    private void navigateTo(String fxmlPath) {
+        try {
+            loadingAnchorpane.getChildren().clear();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            AnchorPane loadedPane = loader.load();
+            loadingAnchorpane.getChildren().add(loadedPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Failed to load view: " + fxmlPath);
+        }
+        System.out.println("Loading FXML: " + fxmlPath);
     }
 
 }
