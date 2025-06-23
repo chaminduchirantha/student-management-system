c936d623e44b88608635cd308b22b5ac2b78d0da
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-06-23 21:55:55 +0530

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module lk.ijse.gdse.studentmanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires static lombok;
+    requires java.desktop;
 
 
     opens lk.ijse.gdse.studentmanagementsystem.controller to javafx.fxml;
