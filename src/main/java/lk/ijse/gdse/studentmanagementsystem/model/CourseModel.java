73f22c0db85a39aa5dd74c13cd7ce1095f53188a
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-06-30 21:53:54 +0530

    update the course model

--- src/main/java/lk/ijse/gdse/studentmanagementsystem/model/CourseModel.java
+++ src/main/java/lk/ijse/gdse/studentmanagementsystem/model/CourseModel.java
@@ -17,7 +17,7 @@ public class CourseModel {
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, courseDto.getCourseId());
         preparedStatement.setString(2, courseDto.getCourseName());
-        preparedStatement.setDouble(3,courseDto.getCoursePrice());
+        preparedStatement.setString(3,courseDto.getCoursePrice());
         preparedStatement.setString(4,courseDto.getCourseDuration());
         preparedStatement.setString(5,courseDto.getStudentId());
 
@@ -28,23 +28,23 @@ public class CourseModel {
 
     public boolean updateCourse(CourseDto courseDto) throws SQLException, ClassNotFoundException {
         Connection connection = DbConnection.getInstance().getConnection();
-        String sql = "UPDATE student SET course_name = ?, course_payment = ?, student_birth_day = ?, course_diuration = ?, student_id = ? WHERE course_id = ?";
+        String sql = "UPDATE course SET course_name = ?, course_payment = ?, student_birth_day = ?, course_diuration = ?, student_id = ? WHERE course_id = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, courseDto.getCourseName());
-        preparedStatement.setDouble(2,courseDto.getCoursePrice());
+        preparedStatement.setString(2,courseDto.getCoursePrice());
         preparedStatement.setString(3,courseDto.getCourseDuration());
         preparedStatement.setString(4,courseDto.getStudentId());
         preparedStatement.setString(5, courseDto.getCourseId());
 
 
         int i = preparedStatement.executeUpdate();
-        boolean isSaved =  i > 0;
-        return isSaved;
+        boolean isUpdate =  i > 0;
+        return isUpdate;
     }
 
     public boolean deleteCourse(String courseId) throws SQLException, ClassNotFoundException {
         Connection connection = DbConnection.getInstance().getConnection();
-        String sql = "delete from student where course_id = ?";
+        String sql = "delete from course where course_id = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, courseId);
 
@@ -55,7 +55,7 @@ public class CourseModel {
 
     public List<CourseDto> getAllCourses() throws SQLException, ClassNotFoundException {
         Connection connection = DbConnection.getInstance().getConnection();
-        String sql = "SELECT * FROM student"; // change 'student' to 'course' if needed
+        String sql = "SELECT * FROM course"; // change 'student' to 'course' if needed
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -65,7 +65,7 @@ public class CourseModel {
             CourseDto courseDto = new CourseDto(
                     resultSet.getString("course_id"),
                     resultSet.getString("course_name"),
-                    resultSet.getDouble("course_payment"),
+                    resultSet.getString("course_payment"),
                     resultSet.getString("course_diuration"),
                     resultSet.getString("student_id")
             );
